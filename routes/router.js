319cab0252c1646818e7f11029a4c8cbfb67bd4e
Ubuntu <ubuntu@ip-172-31-24-97.ap-south-1.compute.internal>
2025-07-19 16:38:22 +0000

    Initialize project structure with TypeScript and Express setup

--- routes/router.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { testDocker, codeRunner } = require('../controller/runCode');
-const router = express.Router();
-
-router.post('/', codeRunner);
-router.get('/test', testDocker);
-
-module.exports = router;
