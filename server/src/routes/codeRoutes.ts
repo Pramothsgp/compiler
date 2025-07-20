444048a267a1f4a34e8c2b03e503f1af22dd592a
Pramothsgp <jarvis.tech.006@gmail.com>
2025-07-20 03:25:57 +0000

    Update /api/code routes to apply auth middleware

--- server/src/routes/codeRoutes.ts
+++ server/src/routes/codeRoutes.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import { compile } from '../controller/compileCode';
+import { rateLimiter } from '../middleware/rateLimiter';
+import { validateApiKey } from '../middleware/authMiddleware';
 const router = express.Router();
 
-router.post('/compile', compile);
+router.post('/compile', validateApiKey , rateLimiter ,compile);
 
 export default router;
